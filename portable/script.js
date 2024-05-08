0b8c5175f49cd8f62c1d81065dc842213751acde
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-05-08 08:25:10 +0100

    Update script.js

--- portable/script.js
+++ portable/script.js
@@ -9,8 +9,8 @@ document.getElementById("portfolio-container").onmousemove = e => {
 }
 
 function ironDownload() {
-  downlaodFile("./Downloads/IronPortable.zip" "IronPortable.zip")
-  downloadFile("./Downloads/IronPortable.z01", "IronPortable.z01")
+  downlaodFile("./Downloads/" "IronPortable.zip")
+  downloadFile("./Downloads/", "IronPortable.z01")
 }
 
 function downloadFile(url, fileName) {
