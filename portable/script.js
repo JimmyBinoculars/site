cecb92a27944bef387005f33ab29875556f1f202
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-05-08 07:42:17 +0100

    Two files

--- portable/script.js
+++ portable/script.js
@@ -7,3 +7,19 @@ document.getElementById("portfolio-container").onmousemove = e => {
     item.style.setProperty("--mouse-y", `${y}px`)
   }
 }
+
+function downloadFiles() {
+  var file1Url = './Downloads/IronPortable.zip'; // Replace with actual file URL
+  var file2Url = './Downloads/IronPortable.z01'; // Replace with actual file URL
+
+  var link = document.getElementById('downloadLink');
+  link.setAttribute('href', file1Url);
+  link.setAttribute('download', ''); // This attribute is necessary for download
+  link.click();
+
+  // Create a new link for the second file
+  var link2 = document.createElement('a');
+  link2.setAttribute('href', file2Url);
+  link2.setAttribute('download', ''); // This attribute is necessary for download
+  link2.click();
+}
